a245630c15ed58c764cc0cbd8fa19310db633815
ThienNN <thiennn2105@gmail.com>
2022-12-10 00:18:19 +0700

    set_up travis

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=123456789
+export POSTGRES_HOST=cdr.ca9zrssvrtvq.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::proj3-bucket-183750537237
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
---
+++ udagram-api-user/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'postgres',
+  'password': '123456789',
+  'database': 'postgres',
+  'host': 'cdr.ca9zrssvrtvq.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'arn:aws:s3:::proj3-bucket-183750537237',
+  'url': 'http://localhost:8100',
+  'jwt': {
+    'secret': 'testing',
+  },
+};
